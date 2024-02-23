cd677d4af124ae50723d10c755c3fd1ae70d169d
“Nader <nadmot23@gmail.com>
2024-02-23 18:58:08 +0000

    Finished modal, added button functionality, started submit functionality

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+// const entry: Entry = {
+//     timeEvent: $sortSelect.value,
+//     dayEvent: '',
+//     information: '',
+//   }
 var $addNewEvent = document.querySelector('#add-event-button');
 var $sortSelect = document.querySelector('#day-select');
+var $dialogAdd = document.querySelector('.dialog-add');
+var $dismissModal = document.querySelector(".dismiss-modal");
 $addNewEvent.addEventListener('click', function () {
-    var entry = {
-        timeEvent: $sortSelect.value,
-        dayEvent: '',
-        information: '',
-    };
+    $dialogAdd.showModal();
+});
+$dismissModal.addEventListener('click', function () {
+    $dialogAdd.close();
 });
--- main.ts
+++ main.ts
@@ -4,15 +4,22 @@ interface Entry {
   information: string;
 }
 
+// const entry: Entry = {
+//     timeEvent: $sortSelect.value,
+//     dayEvent: '',
+//     information: '',
+
+//   }
 
 const $addNewEvent = document.querySelector('#add-event-button');
 const $sortSelect = document.querySelector('#day-select') as HTMLSelectElement;
+const $dialogAdd = document.querySelector('.dialog-add') as HTMLDialogElement;
+const $dismissModal = document.querySelector(".dismiss-modal") as HTMLButtonElement;
 
 $addNewEvent.addEventListener('click', () => {
-  const entry: Entry = {
-    timeEvent: $sortSelect.value,
-    dayEvent: '',
-    information: '',
-
-  }
+  $dialogAdd.showModal();
 })
+
+$dismissModal.addEventListener('click', () => {
+  $dialogAdd.close();
+});
