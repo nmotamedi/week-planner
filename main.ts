3841a0d1fef58f4278d3e17d3f75b478862424d4
“Nader <nadmot23@gmail.com>
2024-02-23 19:00:12 +0000

    updated main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-interface Entry {
+interface Entry extends HTMLFormControlsCollection{
   timeEvent: string;
   dayEvent: string;
   information: string;
@@ -10,11 +10,13 @@ interface Entry {
 //     information: '',
 
 //   }
-
+const $eventForm = document.querySelector("#event-form") as HTMLFormElement;
 const $addNewEvent = document.querySelector('#add-event-button');
 const $sortSelect = document.querySelector('#day-select') as HTMLSelectElement;
 const $dialogAdd = document.querySelector('.dialog-add') as HTMLDialogElement;
 const $dismissModal = document.querySelector(".dismiss-modal") as HTMLButtonElement;
+const $submitModal = document.querySelector(".accept-modal") as HTMLButtonElement;
+
 
 $addNewEvent.addEventListener('click', () => {
   $dialogAdd.showModal();
@@ -23,3 +25,9 @@ $addNewEvent.addEventListener('click', () => {
 $dismissModal.addEventListener('click', () => {
   $dialogAdd.close();
 });
+
+$eventForm.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+  const $formElements = $eventForm.elements as Entry;
+
+})
