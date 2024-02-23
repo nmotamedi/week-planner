67306c0b59bfcbf864aa7a25a362d379a883836b
William Zhang <willzhang73@gmail.com>
2024-02-23 19:18:52 +0000

    entryObject created, index track added

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
+var entryID = 1;
+var entryArray = [];
 // const entry: Entry = {
 //     timeEvent: $sortSelect.value,
 //     dayEvent: '',
 //     information: '',
 //   }
+var $eventForm = document.querySelector('#event-form');
 var $addNewEvent = document.querySelector('#add-event-button');
 var $sortSelect = document.querySelector('#day-select');
 var $dialogAdd = document.querySelector('.dialog-add');
-var $dismissModal = document.querySelector(".dismiss-modal");
+var $dismissModal = document.querySelector('.dismiss-modal');
+var $submitModal = document.querySelector('.accept-modal');
 $addNewEvent.addEventListener('click', function () {
     $dialogAdd.showModal();
 });
 $dismissModal.addEventListener('click', function () {
     $dialogAdd.close();
+});
+$eventForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var $formElements = $eventForm.elements;
+    var timeEvent = $formElements.timeEvent.value;
+    var dayEvent = $formElements.dayEvent.value;
+    var information = $formElements.information.value;
+    entryID++;
+    var entry = {
+        timeEvent: timeEvent,
+        dayEvent: dayEvent,
+        information: information,
+        entryID: entryID,
+    };
+    entryArray.push(entry);
+    console.log('entry: ', entry);
+    $dialogAdd.close();
+    $eventForm.reset();
 });
--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-interface Entry extends HTMLFormControlsCollection{
+interface FormElements extends HTMLFormControlsCollection {
+  timeEvent: HTMLSelectElement;
+  dayEvent: HTMLSelectElement;
+  information: HTMLTextAreaElement;
+}
+
+interface Entry {
   timeEvent: string;
   dayEvent: string;
   information: string;
+  entryID: number;
 }
+
+let entryID = 1;
+
+const entryArray: Entry[] = [];
 
 // const entry: Entry = {
 //     timeEvent: $sortSelect.value,
@@ -10,24 +21,42 @@ interface Entry extends HTMLFormControlsCollection{
 //     information: '',
 
 //   }
-const $eventForm = document.querySelector("#event-form") as HTMLFormElement;
+const $eventForm = document.querySelector('#event-form') as HTMLFormElement;
 const $addNewEvent = document.querySelector('#add-event-button');
 const $sortSelect = document.querySelector('#day-select') as HTMLSelectElement;
 const $dialogAdd = document.querySelector('.dialog-add') as HTMLDialogElement;
-const $dismissModal = document.querySelector(".dismiss-modal") as HTMLButtonElement;
-const $submitModal = document.querySelector(".accept-modal") as HTMLButtonElement;
-
+const $dismissModal = document.querySelector(
+  '.dismiss-modal',
+) as HTMLButtonElement;
+const $submitModal = document.querySelector(
+  '.accept-modal',
+) as HTMLButtonElement;
 
 $addNewEvent.addEventListener('click', () => {
   $dialogAdd.showModal();
-})
+});
 
 $dismissModal.addEventListener('click', () => {
   $dialogAdd.close();
 });
 
-$eventForm.addEventListener("submit", (event: Event) => {
+$eventForm.addEventListener('submit', (event: Event) => {
   event.preventDefault();
-  const $formElements = $eventForm.elements as Entry;
+  const $formElements = $eventForm.elements as FormElements;
+  const timeEvent = $formElements.timeEvent.value;
+  const dayEvent = $formElements.dayEvent.value;
+  const information = $formElements.information.value;
+  entryID++;
+  const entry = {
+    timeEvent,
+    dayEvent,
+    information,
+    entryID,
+  };
 
-})
+  entryArray.push(entry);
+
+  console.log('entry: ', entry);
+  $dialogAdd.close();
+  $eventForm.reset();
+});
